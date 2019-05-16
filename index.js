66e1a1bfe0bbf93d41be808e769fb8fce7a82fa6
v_lzhicliu <v_lzhicliu@tencent.com>
2019-05-16 21:43:25 +0800

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const multer = require('multer');
+const cors = require('cors');
+const upload = multer({ dest: 'uploads' })
+
+const app = express();
+
+app.options('/upload',cors());
+app.post('/upload', cors(), upload.single('avatar'), (req, res, next) => {
+    res.json({key: req.file.filename})
+})
+
+app.get('/preview/:key', cors(), (req, res, next) => {
+    let { key } = req.params;
+    res.sendFile(`uploads/${key}`,{
+        root: __dirname,
+        headers: {
+            'Content-Type': 'image/jepg'
+        }
+    },(error) => {
+        res.status(404).send('Not Found')
+    })
+})
+
+app.listen(3000 ,function(){
+    console.log('APP listening on port 3000')
+})
