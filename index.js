8297c49f8c29a74128e02e3c9481282de2f1c631
shenfeng1945 <823902826@qq.com>
2019-09-26 11:29:11 +0800

    start https server

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const multer = require('multer');
 const cors = require('cors');
 const upload = multer({ dest: 'uploads' })
+const https = require('https')
+const fs = require('fs')
 
 const app = express();
 
@@ -27,6 +29,13 @@ app.get('/preview/:key', cors(), (req, res) => {
     })
 })
 
-app.listen( process.env.PORT || 3000 ,function(){
+const key = fs.readFileSync('/usr/local/nginx/conf/2_shenfeng1945.cn.key','utf8');
+var cert = fs.readFileSync('/usr/local/nginx/conf/1_shenfeng1945.cn_bundle.crt','utf8');
+var options = {
+    key: key,
+    cert: cert
+};
+
+https.createServer(options, app).listen( process.env.PORT || 3000 ,function(){
     console.log('APP listening on port' + ' ' + (process.env.PORT || 3000))
 })
